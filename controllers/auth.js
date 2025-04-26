0fc3fcd88eae2e7b6a3b526245a9d0f7abf88e87
Leonard <leonardglez12485@gmail.com>
2025-04-26 00:51:46 -0300

    fix: corregir la importación del modelo User en el controlador de autenticación

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require("../models/User");
 const { generateJWT } = require("../helpers/jwt");
 
 const createUser = async (req, res = response) => {
